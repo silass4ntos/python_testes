dfb50a21d91ad7a3409e521b3a1e8bbae2e7ce42
Silas Santos <93554313+Silassantosmoura@users.noreply.github.com>
2023-01-16 14:54:00 -0300

    Add files via upload

---
+++ python/GerarPin.py
@@ -0,0 +1,18 @@
+import random
+
+a = int(input("insira um numero: "))
+
+
+
+for i in range (a):
+    print('|-----------------|')
+
+    s = print('|',
+    random.randint(0,9),random.randint(0,9),
+    random.randint(0,9),random.randint(0,9),
+    random.randint(0,9),random.randint(0,9),
+    random.randint(0,9),random.randint(0,9),'|')
+
+print('|-----------------|')
+
+    
